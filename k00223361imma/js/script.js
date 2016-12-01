a6bd8dacc496a90a5d220c57b7eb5f9077e0358c
dragonboyvny - bitbucket <vinnylee@gmail.com>
2016-12-01 00:40:46 +0000

    fixed most Nav issues, footer, overall layout in place
    To Do:
    modal,
    contact
    local store
    carousel
    parallax
    canvas
    6 more pages

--- k00223361imma/js/script.js
+++ k00223361imma/js/script.js
@@ -1,15 +1,63 @@
 $(document).ready(function($){
+	//scroll
 	$(window).scroll(function() {
 		$scroll = $(document).scrollTop();
 		console.log($scroll);
 
 	  	if ($(document).scrollTop() > 50) {
-	    	$('.navbar').addClass('shrink');
+	    	$('.navbar-default').addClass('shrink');
+	    	$('.brand').addClass('shrink');
+	    	$('.navWrapper').addClass('shrink');
+	    	
 	  	} else {
-	    	$('.navbar').removeClass('shrink');
+	    	$('.navbar-default').removeClass('shrink');
+	    	$('.brand').removeClass('shrink');
+	    	$('.navWrapper').removeClass('shrink');
 	  	}
 	});
+/*
+	//GoogleMaps
+	function initMap(location) {
+		var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coordslongtitude);
+		
+		var mapOptions = {
+			center: new google.maps.LatLng(53.343096, -6.300037),
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}; //centred at IMMA location
+
+		var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+		
+		var marker = new google.maps.Marker({
+			position: currentLocation,
+			map: map
+		}); //places marker at our current position
+		if (navigator.geoLocation){ 
+			console.log(location); 
+			navigator.geoLocation.getCurrentPosition(initMap);	
+		} else (x.innerHTML="Geolocation is not supported by this browser.";)
+
+		function showPosition(position){
+			x.innerHTML="Latitude: " + position.coords.latitude +
+				"<br>Longitude: " + position.coords.longitude;
+		} //end showPosition
+	} //end initMap 
+*/
+
 });
+
+//GoogleMaps
+
+/*	function initMap(){
+		var mapOptions = {
+			center: new google.maps.LatLng(53.343096, -6.300037),
+			zoom: 12,
+			scrollwheel: false,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}; //centred at IMMA location
+		var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	} //end GoogleMaps
+google.maps.event.addDomListener(window, "load", initMap);*/
 
 
 
